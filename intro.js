b3b1599f2780fe728953271685f7dd61ec4aab84
Jomaré <105128755+Jomare22@users.noreply.github.com>
2022-06-23 00:52:42 +0200

    Initial commit

---
+++ intro.js
@@ -0,0 +1,5 @@
+let myName = 'Jomare';
+let myHeight = '1,63 m';
+let myCountry = 'South-Africa';
+let printMsg = "My name: " + myName + "\nMy height: " + myHeight + "\nMy country: " + myCountry;
+alert(printMsg);
